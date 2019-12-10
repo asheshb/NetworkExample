65dc5d363400d5519484342c6c47f0a41d4b218e
Ashesh Bharadwaj <ppashesh@gmail.com>
2019-12-10 22:23:00 +0530

    Initial Commit

--- app/src/main/java/com/example/networkexample/data/MovieListDao.kt
+++ app/src/main/java/com/example/networkexample/data/MovieListDao.kt
@@ -2,11 +2,16 @@ package com.example.networkexample.data
 
 import androidx.lifecycle.LiveData
 import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
 import androidx.room.Query
 
 @Dao
 interface MovieListDao{
     @Query("SELECT * FROM movie ORDER BY release_date DESC")
     fun getMovies(): LiveData<List<Movie>>
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun insertMovies(movies: List<Movie>)
 
 }
