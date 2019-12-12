3e8f9adea421b6dd168dde6b5d790dcfcc66f04c
Ashesh Bharadwaj <ppashesh@gmail.com>
2019-12-12 23:07:35 +0530

    Initial Commit

--- app/src/main/java/com/example/networkexample/data/network/TmdbService.kt
+++ app/src/main/java/com/example/networkexample/data/network/TmdbService.kt
@@ -19,26 +19,28 @@ interface TmdbService{
     companion object {
         private const val BASE_URL = "https://api.themoviedb.org/3/"
 
-        // Add api key to every request
-        private val interceptor = Interceptor { chain ->
-            val request = chain.request()
-            val url = request.url().newBuilder()
-                .addQueryParameter("api_key", BuildConfig.TMDB_API_KEY)
-                .build()
-            val newRequest = request.newBuilder()
-                .url(url)
-                .build()
-            chain.proceed(newRequest)
-
-        }
-
-        private val httpClient = OkHttpClient().newBuilder().addInterceptor(interceptor).build()
-
-        private val gson = GsonBuilder()
-            .setDateFormat("yyyy-MM-dd HH:mm:ss")
-            .create()
 
         private val retrofitService by lazy {
+
+            // Add api key to every request
+            val interceptor = Interceptor { chain ->
+                val request = chain.request()
+                val url = request.url().newBuilder()
+                    .addQueryParameter("api_key", BuildConfig.TMDB_API_KEY)
+                    .build()
+                val newRequest = request.newBuilder()
+                    .url(url)
+                    .build()
+                chain.proceed(newRequest)
+
+            }
+
+            val httpClient = OkHttpClient().newBuilder().addInterceptor(interceptor).build()
+
+            val gson = GsonBuilder()
+                .setDateFormat("yyyy-MM-dd HH:mm:ss")
+                .create()
+
             Retrofit.Builder()
                 .baseUrl(BASE_URL)
                 .client(httpClient)
