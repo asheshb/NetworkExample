ec28a3941ca0989f70aef97d11eb59ace81f6f4b
Ashesh Bharadwaj <ppashesh@gmail.com>
2019-12-12 21:56:04 +0530

    Initial Commit

--- app/src/main/java/com/example/networkexample/data/Movie.kt
+++ app/src/main/java/com/example/networkexample/data/Movie.kt
@@ -5,9 +5,6 @@ import androidx.room.Entity
 import androidx.room.PrimaryKey
 import java.util.*
 
-//https://developers.themoviedb.org/3/discover/movie-discover
-//https://api.themoviedb.org/3/discover/movie?api_key=fd2e2649af1655e26b1049940615a98b
-// &language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1
 @Entity(tableName = "movie")
 data class Movie(
     @PrimaryKey
