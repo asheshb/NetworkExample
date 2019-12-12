9663986e1e267724c9fc73179bb66ab15a2ffe7a
Ashesh Bharadwaj <ppashesh@gmail.com>
2019-12-12 23:51:25 +0530

    Initial Commit

--- app/src/main/java/com/example/networkexample/data/network/TmdbService.kt
+++ app/src/main/java/com/example/networkexample/data/network/TmdbService.kt
@@ -18,8 +18,8 @@ interface TmdbService{
 
     companion object {
         private const val BASE_URL = "https://api.themoviedb.org/3/"
-        private const val POSTER_BASE_URL = "https://image.tmdb.org/t/p/w185"
-        private const val BACKDROP_BASE_URL = "https://image.tmdb.org/t/p/w300"
+        const val POSTER_BASE_URL = "https://image.tmdb.org/t/p/w185"
+        const val BACKDROP_BASE_URL = "https://image.tmdb.org/t/p/w300"
 
         private val retrofitService by lazy {
             // Add api key to every request
